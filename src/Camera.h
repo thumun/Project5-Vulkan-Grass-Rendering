98a77cb74f9cadb3c90444d0f96782d9ab45da41
Neha Thumu <71038542+thumun@users.noreply.github.com>
2025-10-29 22:47:24 -0400

    release data

--- src/Camera.h
+++ src/Camera.h
@@ -7,13 +7,14 @@
 struct CameraBufferObject {
   glm::mat4 viewMatrix;
   glm::mat4 projectionMatrix;
-  uint32_t tessellationLODEnabled = 0;
+  uint32_t tessellationLODEnabled = 1;
   uint32_t renderSettingEnableRecovery = 1;
   float renderSettingEnableGravity = 9.8f;
   uint32_t renderSettingEnableWind = 1;
-  float orientationCullingThreshold = 0.9f; // zero means disabled
-  float viewFrustumCullingEnabled = 0.2f;// zero means disabled
-  float distanceCullingThreshold = 15.0f; // zero means disabled
+  // 0 to disable the below
+  float orientationCullingThreshold = 0.9f;
+  float viewFrustumCullingEnabled = 0.2f;
+  float distanceCullingThreshold = 15.0f;
 };
 
 class Camera {
